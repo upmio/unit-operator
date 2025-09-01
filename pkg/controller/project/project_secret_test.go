67dda9a018856f2aea2792239dbfcf37360a5dff
chenxiinn <chenxiinn@gmail.com>
2025-09-01 17:01:54 +0800

    [UPDATE] project: update unit test

--- pkg/controller/project/project_secret_test.go
+++ pkg/controller/project/project_secret_test.go
@@ -5,15 +5,15 @@ import "testing"
 func TestGenerateAES256Key(t *testing.T) {
 	key, err := generateAES256Key()
 	if err != nil {
-		t.Fatalf("generateAES256Key 返回错误: %v", err)
+		t.Fatalf("generateAES256Key fail: %v", err)
 	}
-	
+
 	if len(key) != 32 {
 
-		t.Errorf("生成的 key 长度不是 32，实际为: %d", len(key))
+		t.Errorf("the generated key length is not 32, actual: %d", len(key))
 	}
-	// 检查是否为有效的 32 字节十六进制字符串
+	// Check if it is a valid 32-byte hexadecimal string
 	if !isValidHex32(key) {
-		t.Errorf("生成的 key 不是有效的 32 字节十六进制字符串: %s", string(key))
+		t.Errorf("the generated key is not a valid 32-byte hexadecimal string: %s", string(key))
 	}
 }
